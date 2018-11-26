430acaa7e4cd56487392573a53da9cf388f6f6f1
Sana Iraqui <sanairaqui1999@gmail.com>
2018-11-26 12:47:18 +0530

    component.js file use in paysack website

---
+++ src/routes/home/components/feature-grid/component.js
@@ -0,0 +1,22 @@
+let data = require("./data")
+let list_map = data.list_map;
+
+module.exports = class {
+    onCreate() {
+        this.state = {
+            first_heading: list_map.first.data.heading,
+            second_heading: list_map.second.data.heading,
+            third_heading: list_map.third.data.heading,
+            forth_heading: list_map.forth.data.heading,
+
+            first_paragraph: list_map.first.data.paragraph,
+            second_paragraph: list_map.second.data.paragraph,
+            third_paragraph: list_map.third.data.paragraph,
+            forth_heading: list_map.forth.data.paragraph
+        }
+
+
+    }
+
+}
+
---
+++ src/routes/home/components/feature-grid/data.js
@@ -0,0 +1,29 @@
+let list_map = {
+
+    "first": {
+        "data": {
+            "heading": "LIVE WELL",
+            "paragraph": "Employee self-service and seamless wellness management. Physical fitness, or emotional well-being, we have your back."
+        }
+    },
+    "second": {
+        "data": {
+            "heading": "INSURANCE HUB",
+            "paragraph": "Navigate the otherwise complicated processes, information, claims and other resources. All insurance data in one place."
+        }
+    },
+    "third": {
+        "data": {
+            "heading": "WALLETS & REWARDS",
+            "paragraph": "The most flexible spending account an employee can ask for. Helps employees save on taxes and submit claims too."
+        }
+    },
+    "forth": {
+        "data": {
+            "heading": "LEARN & EARN",
+            "paragraph": "Employee engagement redefined. For organisations that believe Learning & Development of an employee is crucial.s"
+        }
+
+    }
+}
+exports.list_map = list_map;
